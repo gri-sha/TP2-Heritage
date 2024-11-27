97c7ea0536d104c20c95f4195e6c12ed8d24d161
Claire Prevost <72880999+clairette22@users.noreply.github.com>
2024-11-27 16:03:32 +0100

    change class trajet

---
+++ trajet.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include "trajet.h"
+using namespace std;
+
+
+Trajet::Trajet(const char* pVilleDep, const char* pVilleArr) {
+    villeDep = new char[strlen(pVilleDep)+1];
+    strcpy(villeDep, pVilleDep);
+    villeArr = new char[strlen(pVilleArr)+1];
+    strcpy(villeArr, pVilleArr);
+}
+
+virtual Trajet::~Trajet() {
+    delete[] villeDep;
+    delete[] villeArr;
+}
+
+void Trajet::Afficher() const {
+    cout << "de " << villeDep << " a " << villeArr < " ";
+}
