14774cb4c516dc4bca78c9bc5a62e2bb221f8161
gri-sha <183693051+gri-sha@users.noreply.github.com>
2024-12-08 22:39:23 +0100

    small changes

--- trajet.h
+++ trajet.h
@@ -1,7 +1,5 @@
 #ifndef TRAJET_H
 #define TRAJET_H
-#include <istream>
-#include <cstring>
 
 class Trajet
 {
--- trajetSimple.cpp
+++ trajetSimple.cpp
@@ -7,19 +7,15 @@ TrajetSimple::TrajetSimple(const char *villeDep, const char *villeArr, const cha
 {
     moyen = new char[strlen(moyenTransport) + 1];
     strcpy(moyen, moyenTransport);
-    // cout << "Construction TrajetSimple" << "\r\n";
 }
 
 TrajetSimple::~TrajetSimple()
 {
     delete[] moyen;
-    // cout << "Destruction TrajetSimple" << "\r\n";
 }
 
 void TrajetSimple::afficher() const
 {
-    // cout<<"Trajet simple : \r\n";
     Trajet::afficher();
     cout << "en " << moyen << " ";
-    // cout<<"\r\n";
 }
